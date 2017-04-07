a5ec2736a420f600c42f95820ae9a1e4d0de18dd
Unknown <xvillanueva@mail.csuchico.edu>
2017-04-06 20:42:05 -0800

    ghostwriter first update

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,10 +1,27 @@
 function writeTweet()
 {
-  
-}
+  var randIndex = document.getElementById("tweet").value
+  var sentence = intro[randIndex];
 
+  var intro = ["The unexplainable compliments",
+  "Existance unfolds""Your body transcends" "Imagination comprehends"  ]
+
+var body = ["Storm in a teacup""point blank"
+"through the eye of a needle" "Mad as a hatter" "Sweet fanny Adams"]
+
+var end = ["how strange when an illusion dies.""money is a strange business."]
+}
 //Generates a random whole number between 0 and max (inclusive)
 function randomUpTo(max)
 {
   return Math.floor(Math.random() * (max+1));
 }
+
+var randIndex = randomUpTo(intro.length-1);
+
+
+var randIndex = randomUpTo(body.length-1)
+
+
+var randIndex = randomUpTo(end.length-1)
+;
